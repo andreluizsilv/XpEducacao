7beb24b4e8db8773aabc30a3f9abf3f41a76225e
andreluizsilv <andre_luiz29@hotmail.com>
2023-04-17 17:01:05 -0300

    Aula01

---
+++ Capitulo01/Aula04.py
@@ -0,0 +1,11 @@
+nome = 'Antônio Carlos'
+print('Olá', nome, 'seja bem-vindo!')
+
+pessoa = 'Andre Luiz'
+print('Ola', pessoa, 'Seja Bem Vindo ao BootCamp')
+
+numero = 1
+numero =+ 2
+print(numero)
+
+
